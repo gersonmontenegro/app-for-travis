9d380859d5ce3025f4cf48daa94dc1ef26f9c3b0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-10 15:39:59 +0000

    Deploy gersonmontenegro/app-for-travis to github.com/gersonmontenegro/app-for-travis.git:gh-pages

---
+++ precache-manifest.d5a4a3ce6336bc6ecfe11f0e6c59ac85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c17a4673af7137500d1724a851d172e",
+    "url": "/app-for-travis/index.html"
+  },
+  {
+    "revision": "8c332d648afdcab0aac3",
+    "url": "/app-for-travis/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/app-for-travis/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8c332d648afdcab0aac3",
+    "url": "/app-for-travis/static/js/main.6a94148c.chunk.js"
+  },
+  {
+    "revision": "153ea856b7326ca3d4d1",
+    "url": "/app-for-travis/static/js/runtime~main.0af9c6b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-for-travis/static/media/logo.5d5d9eef.svg"
+  }
+]);
